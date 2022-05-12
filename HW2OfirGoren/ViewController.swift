481c89913129fd42029c7d027fd66892202776fb
OfirGoren <Ofiiir19@gmail.com>
2022-05-12 10:20:42 +0300

    Initial Commit

---
+++ HW2OfirGoren/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2OfirGoren
+//
+//  Created by Ofir Goren on 12/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
